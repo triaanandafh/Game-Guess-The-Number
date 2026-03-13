84ee29e1d9c5d7a3199b36233f3dcb066fbded41
triaanandafh <triafadillah1@gmail.com>
2026-03-13 16:03:51 +0700

    Setup variable

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,11 @@
+import 'dart:math';
+
+void main() {
+  var random = Random();
+  int angkaRahasia = random.nextInt(100) + 1; // Angka 1-100
+  int tebakan = 0;
+  int nyawa = 5;
+  print("=== Game Tebak Angka (1-100) ===");
+
+  
+}

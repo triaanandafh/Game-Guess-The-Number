83216116ea79caf9c4d3314094c1aeaa85ed926a
triaanandafh <triafadillah1@gmail.com>
2026-03-14 10:56:27 +0700

    revisi initialisasi variable

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,11 +1,12 @@
+import 'dart:io';
 import 'dart:math';
 
 void main() {
   var random = Random();
-  int angkaRahasia = random.nextInt(100) + 1; // Angka 1-100
+  int angkaRahasia = random.nextInt(10) + 1; // Angka 1-10
   int tebakan = 0;
   int nyawa = 5;
-  print("=== Game Tebak Angka (1-100) ===");
+  print("=== Game Tebak Angka (1-10) ===");
 
   
 }
